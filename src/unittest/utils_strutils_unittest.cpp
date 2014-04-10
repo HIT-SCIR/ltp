8d056f55aa537ccf3525fe4074e8ebaa6555723a
Oneplus <oneplus.lau@gmail.com>
2014-04-10 14:53:49 +0800

    add unittest case

--- src/unittest/utils_strutils_unittest.cpp
+++ src/unittest/utils_strutils_unittest.cpp
@@ -51,7 +51,7 @@ TEST(strutils_unittest, test_split_leading_ending_space) {
   EXPECT_EQ(tokens.size(), 3);
   EXPECT_STREQ("a", tokens[0].c_str());
   EXPECT_STREQ("b", tokens[1].c_str());
-  EXPECT_STREQ("c", tokens[1].c_str());
+  EXPECT_STREQ("c", tokens[2].c_str());
 }
 
 // I am not quite sure about supporting this case, since each string
@@ -59,6 +59,41 @@ TEST(strutils_unittest, test_split_leading_ending_space) {
 TEST(strutils_unittest, test_split_space) {
   std::vector<std::string> tokens = ltp::strutils::split("  ");
   EXPECT_EQ(tokens.size(), 0);
+}
+
+TEST(strutils_unittest, test_split_limit_num) {
+  std::vector<std::string> tokens = ltp::strutils::split("a b c", 1);
+  EXPECT_EQ(tokens.size(), 2);
+  EXPECT_STREQ("a", tokens[0].c_str());
+  EXPECT_STREQ("b c", tokens[1].c_str());
+}
+
+TEST(strutils_unittest, test_split_by_sep) {
+  std::vector<std::string> tokens = ltp::strutils::split_by_sep("a-b-c", "-");
+  EXPECT_EQ(tokens.size(), 3);
+  EXPECT_STREQ("a", tokens[0].c_str());
+  EXPECT_STREQ("b", tokens[1].c_str());
+  EXPECT_STREQ("c", tokens[2].c_str());
+}
+
+TEST(strutils_unittest, test_split_by_sep_nonexist) {
+  std::vector<std::string> tokens = ltp::strutils::split_by_sep("a-b-c", "|");
+  EXPECT_EQ(tokens.size(), 1);
+  EXPECT_STREQ("a-b-c", tokens[0].c_str());
+}
+
+TEST(strutils_unittest, test_split_by_sep_and_limit_num) {
+  std::vector<std::string> tokens = ltp::strutils::split_by_sep("a-b-c", "-", 1);
+  EXPECT_EQ(tokens.size(), 2);
+  EXPECT_STREQ("a", tokens[0].c_str());
+  EXPECT_STREQ("b-c", tokens[1].c_str());
+}
+
+TEST(strutils_unittest, test_rsplit_limit_num) {
+  std::vector<std::string> tokens = ltp::strutils::rsplit("a b c", 1);
+  EXPECT_EQ(tokens.size(), 2);
+  EXPECT_STREQ("a b", tokens[0].c_str());
+  EXPECT_STREQ("c", tokens[1].c_str());
 }
 
 int main(int argc, char ** argv) {
