82e0c86d4c175811dda195e72445aa2cf3451fbf
Oneplus <oneplus.lau@gmail.com>
2014-05-11 21:46:23 +0800

    add dynamic lib for splitsnt to be compatiable with java and python embedding

--- src/unittest/engpattern_unittest.cpp
+++ src/unittest/engpattern_unittest.cpp
@@ -6,21 +6,21 @@
 
 TEST(engpattern_unittest, english_word) {
   std::string word = "78G"; 
-  EXPECT_EQ(true, boost::regex_match(word,ltp::segmentor::rulebase::engpattern));
+  EXPECT_TRUE(boost::regex_match(word,ltp::segmentor::rulebase::engpattern));
 
   word = "78-d";
-  EXPECT_EQ(true, boost::regex_match(word,ltp::segmentor::rulebase::engpattern));
+  EXPECT_TRUE(boost::regex_match(word,ltp::segmentor::rulebase::engpattern));
 
   word = "md3243";
-  EXPECT_EQ(true, boost::regex_match(word,ltp::segmentor::rulebase::engpattern));
+  EXPECT_TRUE(boost::regex_match(word,ltp::segmentor::rulebase::engpattern));
 
   word = "md-3243";
-  EXPECT_EQ(true, boost::regex_match(word,ltp::segmentor::rulebase::engpattern));
+  EXPECT_TRUE(boost::regex_match(word,ltp::segmentor::rulebase::engpattern));
 }
 
 TEST(engpattern_unittest, number) {
   std::string word = "1997"; 
-  EXPECT_EQ(false, boost::regex_match(word,ltp::segmentor::rulebase::engpattern));
+  EXPECT_FALSE(boost::regex_match(word,ltp::segmentor::rulebase::engpattern));
 }
 
 
