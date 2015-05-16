ad42aa6eb393d4f759c98ab060c9410614940990
Yijia Liu <oneplus.lau@gmail.com>
2015-05-16 09:41:23 +0800

    Addd armadillo header

---
+++ src/config.h
@@ -0,0 +1,12 @@
+#ifndef __LTP_CONFIG_H__
+#define __LTP_CONFIG_H__
+
+#define LTP_VERSION "3.3.0"
+
+#define BOOST_ALL_NO_LIB
+
+#if _MSC_VER
+#define ARMA_USE_WRAPPER
+#endif
+
+#endif  //  end for __LTP_CONFIG_H__
