2404d3327f9834107528ad511675415a3a12d0b2
Oneplus <oneplus.lau@gmail.com>
2015-05-23 11:16:07 +0800

    Revise document version.

--- doc/conf.py
+++ doc/conf.py
@@ -51,7 +51,7 @@ copyright = u'2015, HIT-SCIR'
 # built documents.
 #
 # The short X.Y version.
-version = '3.2'
+version = '3.3'
 # The full version, including alpha/beta/rc tags.
 release = '3.2'
 
