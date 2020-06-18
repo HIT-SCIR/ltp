edd549c62028e1d1c9ed3a71b4ef4fbb1239ee1a
ylfeng <ylfeng@ir.hit.edu.cn>
2020-06-18 12:56:37 +0800

    移除冗余

--- ltp/utils/sent_split.py
+++ ltp/utils/sent_split.py
@@ -3,40 +3,35 @@
 # Author: jeffrey
 # Changed:
 #   sen -> sent / auto strip: ylfeng
-
-
 import re
-import itertools
+from typing import List
 
 
-def split_sentence(document: str, flag: str = "all", limit: int = 512):
+def split_sentence(document: str, flag: str = "all", limit: int = 510) -> List[str]:
     """
 
     Args:
         document:
         flag: Type:str, "all" 中英文标点分句，"zh" 中文标点分句，"en" 英文标点分句
-        limit: 限制最大长度为512个字符
+        limit: 默认单句最大长度为510个字符
 
     Returns: Type:list
 
     """
+    sent_list = []
     try:
         if flag == "zh":
-            document = re.sub('(?P<quotation_mark>([。？！。！？…](?![”’"\'])))', r'\g<quotation_mark>\n', document)  # 单字符断句符
-            document = re.sub('(?P<quotation_mark>([。？！。！？]|…{1,2})[”’"\'])', r'\g<quotation_mark>\n', document)  # 特殊引号
-
+            document = re.sub('(?P<quotation_mark>([。？！…](?![”’"\'])))', r'\g<quotation_mark>\n', document)  # 单字符断句符
+            document = re.sub('(?P<quotation_mark>([。？！]|…{1,2})[”’"\'])', r'\g<quotation_mark>\n', document)  # 特殊引号
         elif flag == "en":
-            document = re.sub('(?P<quotation_mark>([\\.?!](?![”’"\'])))', r'\g<quotation_mark>\n', document)  # 英文单字符断句符
-            document = re.sub('(?P<quotation_mark>([?!\\.]["\']))', r'\g<quotation_mark>\n', document)  # 特殊引号
-
+            document = re.sub('(?P<quotation_mark>([.?!](?![”’"\'])))', r'\g<quotation_mark>\n', document)  # 英文单字符断句符
+            document = re.sub('(?P<quotation_mark>([?!.]["\']))', r'\g<quotation_mark>\n', document)  # 特殊引号
         else:
-            document = re.sub('(?P<quotation_mark>([。？！。！？…\\.?!](?![”’"\'])))', r'\g<quotation_mark>\n',
-                              document)  # 单字符断句符
-            document = re.sub('(?P<quotation_mark>(([。？！。！？\\.!?]|\\…{1,2})[”’"\']))', r'\g<quotation_mark>\n',
+            document = re.sub('(?P<quotation_mark>([。？！….?!](?![”’"\'])))', r'\g<quotation_mark>\n', document)  # 单字符断句符
+            document = re.sub('(?P<quotation_mark>(([。？！.!?]|…{1,2})[”’"\']))', r'\g<quotation_mark>\n',
                               document)  # 特殊引号
 
         sent_list_ori = document.splitlines()
-        sent_list = []
         for sent in sent_list_ori:
             sent = sent.strip()
             if not sent:
