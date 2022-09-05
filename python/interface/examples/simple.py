bd3566e71abeb1fed7c91b6033a443f447601f58
ylfeng <ylfeng@ir.hit.edu.cn>
2022-09-05 20:31:37 +0800

    :art: Update Example

--- python/interface/examples/simple.py
+++ python/interface/examples/simple.py
@@ -3,11 +3,11 @@ from ltp import LTP
 
 def legacy():
     ltp = LTP("LTP/legacy")
+    ltp.add_word("汤姆去")
     result = ltp(
         ["他叫汤姆去拿外衣。", "树上停着一些小鸟。先飞走了19只，又飞走了15只。两次共飞走了多少只小鸟？"],
         tasks=["cws", "pos", "ner"],
     )
-    ltp.add_word("汤姆去")
     print(result.cws)
     print(result.pos)
     print(result.ner)
@@ -41,21 +41,9 @@ def neural():
     print(result.dep)
     print(result.sdp)
 
-    # Batched 未分词的文本
-    result = ltp(
-        ["他叫汤姆去拿外衣。", "韓語：한국의 단오", "树上停着一些小鸟。先飞走了19只，又飞走了15只。两次共飞走了多少只小鸟？"],
-        tasks=["cws", "pos", "ner", "srl", "dep", "sdp"],
-    )
-    print(result.cws)
-    print(result.pos)
-    print(result.ner)
-    print(result.srl)
-    print(result.dep)
-    print(result.sdp)
-
 
 def main():
-    # legacy()
+    legacy()
     neural()
 
 
