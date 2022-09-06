9bf7e1f4812c8732195fd96bc88b2fc361fa1821
ylfeng <ylfeng@ir.hit.edu.cn>
2022-09-06 19:06:15 +0800

    :arrow_up: 升级 pyo3 到 0.17

--- python/extension/src/lib.rs
+++ python/extension/src/lib.rs
@@ -17,44 +17,40 @@ pub use perceptron::{
     PyAlgorithm, PyCWSModel, PyCWSTrainer, PyNERModel, PyNERTrainer, PyPOSModel, PyPOSTrainer,
 };
 use pyo3::prelude::*;
-use pyo3::wrap_pymodule;
 use stnsplit::StnSplit;
 
-/// Algorithms Module
-#[pymodule]
-fn algorithms(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<StnSplit>()?;
-    m.add_class::<PyHook>()?;
-    m.add_function(wrap_pyfunction!(py_eisner, m)?)?;
-    m.add_function(wrap_pyfunction!(py_get_entities, m)?)?;
-    m.add_function(wrap_pyfunction!(py_viterbi_decode_postprocess, m)?)?;
-    Ok(())
-}
 
 /// LTP Module
 #[pymodule]
-fn perceptron(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<PyModel>()?;
-    m.add_class::<ModelType>()?;
-    m.add_class::<PyTrainer>()?;
-    m.add_class::<PyAlgorithm>()?;
-
-    m.add_class::<PyCWSModel>()?;
-    m.add_class::<PyCWSTrainer>()?;
-
-    m.add_class::<PyPOSModel>()?;
-    m.add_class::<PyPOSTrainer>()?;
-
-    m.add_class::<PyNERModel>()?;
-    m.add_class::<PyNERTrainer>()?;
-    Ok(())
-}
-
-/// LTP Module
-#[pymodule]
-fn ltp_extension(_py: Python, m: &PyModule) -> PyResult<()> {
+fn ltp_extension(py: Python, m: &PyModule) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
-    m.add_wrapped(wrap_pymodule!(algorithms))?;
-    m.add_wrapped(wrap_pymodule!(perceptron))?;
+
+    // Algorithms Module
+    let algorithms = PyModule::new(py, "algorithms")?;
+
+    algorithms.add_class::<StnSplit>()?;
+    algorithms.add_class::<PyHook>()?;
+    algorithms.add_function(wrap_pyfunction!(py_eisner, m)?)?;
+    algorithms.add_function(wrap_pyfunction!(py_get_entities, m)?)?;
+    algorithms.add_function(wrap_pyfunction!(py_viterbi_decode_postprocess, m)?)?;
+
+    // Perceptron Module
+    let perceptron = PyModule::new(py, "perceptron")?;
+    perceptron.add_class::<PyModel>()?;
+    perceptron.add_class::<ModelType>()?;
+    perceptron.add_class::<PyTrainer>()?;
+    perceptron.add_class::<PyAlgorithm>()?;
+
+    perceptron.add_class::<PyCWSModel>()?;
+    perceptron.add_class::<PyCWSTrainer>()?;
+
+    perceptron.add_class::<PyPOSModel>()?;
+    perceptron.add_class::<PyPOSTrainer>()?;
+
+    perceptron.add_class::<PyNERModel>()?;
+    perceptron.add_class::<PyNERTrainer>()?;
+
+    m.add_submodule(algorithms)?;
+    m.add_submodule(perceptron)?;
     Ok(())
 }
