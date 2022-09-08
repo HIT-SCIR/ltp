45a76846dc8aa4873a78ce5adcb17bb39c942c1e
ylfeng <ylfeng@ir.hit.edu.cn>
2022-09-08 16:25:22 +0800

    :art: Update Example

--- python/interface/examples/simple.py
+++ python/interface/examples/simple.py
@@ -26,7 +26,7 @@ def neural():
     # 未分词的文本
     result = ltp.pipeline(
         ["他叫汤姆去拿外衣。", "韓語：한국의 단오", "树上停着一些小鸟。先飞走了19只，又飞走了15只。两次共飞走了多少只小鸟？"],
-        tasks=["cws"],
+        tasks=["cws", "pos", "ner", "srl", "dep", "sdp"],
     )
     print(result.cws)
     print(result.pos)
